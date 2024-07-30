eb1e91e5b532ef5934512645da065b31fd0ce7b2
Silvio-Gimenez <srgb.300@gmail.com>
2024-07-29 22:29:15 -0400

    Proyecto creado

---
+++ src/test/java/com/silviogimenez/cursito/CursitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silviogimenez.cursito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
